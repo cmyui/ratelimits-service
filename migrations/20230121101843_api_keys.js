c2c195a6c5bdb3969c91d36f2309238493d9a933
cmyui <cmyuiosu@gmail.com>
2023-01-21 05:53:01 -0500

    feat: api keys database migrations

---
+++ migrations/20230121101843_api_keys.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('api_keys', table => {
+      table.increments('id').primary();
+      table.string('key').notNullable();
+      table.string('name').unique().notNullable();
+      table.enum('status', ['active', 'deleted']).notNullable();
+      table.timestamp('created_at', { useTz: true }).defaultTo(knex.fn.now());
+      table.timestamp('updated_at', { useTz: true }).defaultTo(knex.fn.now());
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('api_keys');
+};
