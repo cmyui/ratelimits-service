51d060b51d62abd6fc1ad7e89816b47ea917e816
cmyui <cmyuiosu@gmail.com>
2023-01-21 05:12:11 -0500

    feat: mysql database setup with knex

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+  },
+
+  staging: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  }
+
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { KnexModule } from 'nestjs-knex';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    KnexModule.forRoot({
+      config: {
+        client: 'mysql2',
+        connection: {
+          host: process.env.DB_HOST!,
+          port: parseInt(process.env.DB_PORT!),
+          user: process.env.DB_USER!,
+          password: process.env.DB_PASS!,
+          database: process.env.DB_NAME!,
+        },
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
